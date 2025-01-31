fb5d060a0c612536e1f068c1816e54e656ee7a1f
Ari Lotter <arilotter@gmail.com>
2025-01-31 15:13:03 -0500

    minimize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bitvec::{array::BitArray, order::Lsb0};
 
 fn main() {
-    let a = BitArray::<[u64; 4], Lsb0>::new([0u64; 4]);
-    dbg!(a);
+    dbg!(BitArray::<[u64; 4], Lsb0>::new([0u64; 4]));
 }
