eafbfe5a04962b0d88c24dd7b5515700d3c5a3f6
Ari Lotter <arilotter@gmail.com>
2025-01-31 15:12:46 -0500

    concrete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bitvec::{array::BitArray, order::Lsb0};
 
 fn main() {
-    let a = BitArray::<_, Lsb0>::new([0u64; 4]);
+    let a = BitArray::<[u64; 4], Lsb0>::new([0u64; 4]);
     dbg!(a);
 }
